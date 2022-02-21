42bbe364fd29849ff981b59eb82ffcacac021d35
VTIvanov20 <vtivanov20@codingburgas.bg>
2022-02-21 16:04:13 +0200

    Initialize Three.js scene, camera and renderer

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#app'),
+  antialias:true
+});
+
+renderer.render(scene, camera);
