d04c54fde050140c9c4880e4e9cc2cea398972ff
AEManov20 <AEManov20@codingburgas.bg>
2022-02-23 13:14:53 +0200

    Reinitialise project in React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
