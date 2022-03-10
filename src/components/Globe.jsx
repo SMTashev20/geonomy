3fe25ab1bec9b29ac6388d74c66b01e41fc98f2c
AEManov20 <AEManov20@codingburgas.bg>
2022-03-10 22:38:37 +0200

    Refactor texture fetching to Promise.all

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -17,51 +17,40 @@ export function Globe(props) {
     const [updateFrame, setUpdateFrame] = useState(false);
 
     // scene init
-    useEffect(async () => {
+    useEffect(() => {
         console.log('scene init');
         bufferScene.current.background = new THREE.Color('#000000');
         bufferCamera.current.position.setZ(0);
 
-        new MapLoader().load(
-            'https://datahub.io/core/geo-countries/r/countries.geojson',
-            loadedMeshes => {
+        Promise.all([
+            new MapLoader().loadAsync('https://datahub.io/core/geo-countries/r/countries.geojson', console.log),
+            new THREE.TextureLoader().loadAsync('./img/earth.jpg', console.log),
+            new THREE.TextureLoader().loadAsync('./img/normal.jpg', console.log)
+        ]).then(res => {
                 const lineGroup = new THREE.Group();
-                lineGroup.name = "countryLines";
+                lineGroup.name = "CountryLines";
 
-                loadedMeshes.forEach(mesh => {
+                res[0].forEach(mesh => {
                     const line = new THREE.Line(
                         mesh,
                         new THREE.LineBasicMaterial({ color: 0xffffff })
                     );
-    
+
                     line.position.setZ(-2924); // - (2048 + 512 + 256 + 64 + 32 + 8 + 4)
                     lineGroup.add(line);
-                });
+                })
 
-                bufferScene.current.add(lineGroup);
-
-                setUpdateFrame(true);
-            },
-            loadEvent => console.log(loadEvent),
-            error => console.error(error)
-        );
-
-        new THREE.TextureLoader().load(
-            './img/day.jpg',
-            loadedTexture => {
                 const mesh = new THREE.Mesh(
-                    new THREE.BoxGeometry(loadedTexture.image.width, loadedTexture.image.height, 1),
-                    new THREE.MeshBasicMaterial({ map: loadedTexture })
-                );
+                    new THREE.BoxGeometry(res[1].image.width, res[1].image.height, 1),
+                    new THREE.MeshStandardMaterial({ map: res[1], normalMap: res[2] })
+                )
 
                 mesh.position.setZ(-2925); // - (2048 + 512 + 256 + 64 + 32 + 8 + 4 + 1)
                 bufferScene.current.add(mesh);
 
                 setUpdateFrame(true);
-            },
-            loadEvent => console.log(loadEvent),
-            error => console.error(error)
-        );
+            }
+        )
         
         {
             const ambientLight = new THREE.AmbientLight([1, 1, 1], .25);
